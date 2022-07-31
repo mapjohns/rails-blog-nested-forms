d17aed06f69d7316d38f7a252ef5824f81b0fe12
Matthew <mattj20@gmail.com>
2022-07-31 06:54:31 -0700

    commit tree 2a86e539bba169263e5bfdb9c0682d2f6c572c6d

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,10 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+  accepts_nested_attributes_for :tags, reject_if: lambda {|attb| attb[:name].blank?}
+  # def tags_attributes=(tags)
+  #   binding.pry
+  #   tags
+  # end
+
 end
